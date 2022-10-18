91dcfbb1783f2bedd9be5aa58aa03dc2650072f5
Arap Too <kibetbryan874@gmail.com>
2022-10-17 23:50:15 -0700

    Fixed betty issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/*Write a C program that prints exactly 
+/*
+ * Write a C program that prints exactly
  * "Programming is like building a multilingual puzzle, followed by a new line.
  * Use the function puts
  * You are not allowed to use printf
- * Your program should end with the value 0 
- * */
+ * Your program should end with the value 0
+ */
 
-int main ()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
