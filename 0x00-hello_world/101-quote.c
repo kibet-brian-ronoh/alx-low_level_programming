0ee25abcf51de0aebee304c5dbd0846c18ec4226
Arap Too <kibetbryan874@gmail.com>
2022-10-18 00:51:23 -0700

    Advanced task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+* Write a C program that prints exactly and that piece
+* of art is useful" - Dora Korpar, 2015-10-19
+* , followed by a new line, to the standard error.
+*/
+
+/**
+ * main - This is the program entry in C
+ *
+ * Description: Program entry
+ * Return: 0 if program exited successfully
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dor
+			a Korpar, 2015-10-19\n");
+	return (1);
+}
