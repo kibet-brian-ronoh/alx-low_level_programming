c0a50e62e7a8561c379cd37d3f2c968b932408c7
Arap Too <kibetbryan874@gmail.com>
2022-10-19 00:35:35 -0700

    Correction task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the program entry in C
+ *
+ * Description: Program entry
+ * Return: 0 if program exited successfully
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
