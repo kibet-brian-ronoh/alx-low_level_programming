bd03af6a2a0fcd7dbdc48a0f8c789dc8f69b0d74
Arap Too <kibetbryan874@gmail.com>
2022-10-27 13:09:23 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+/**
+ * fizz_buzz - prints the numbers from 0 - 100
+ * but with multiples of three replaced with 'Fizz' and multiples of
+ * 5 replaced with 'Buzz' and multiples of both 3 and 5 replaced with FizzBuzz
+ * Return: Void
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i + 1);
+		}
+		i++;
+	}
+}
